090e7528bed4b507e625516880c212653b07a561
LucasYan-24 <99996509+LucasYan-24@users.noreply.github.com>
2022-02-18 17:50:40 -0300

    Add files via upload

---
+++ Questão2 .js
@@ -0,0 +1,8 @@
+function verificaForcaSenha() 
+    var caracteres = /([      ])/;
+	var numeros = /([0-9])/;
+	var alfabeto = /([a-zA-Z])/;
+	var chEspeciais = /([ !,@,#,$,%,^,&,*,(,),-,+])/;
+
+
+	
